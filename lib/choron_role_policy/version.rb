97a1fa1cfa6ea87f235a31cccde6cf8a5c00cd2a
mksava <dosec.mk@gmail.com>
2023-05-21 20:59:18 +0900

    first commit

---
+++ lib/choron_role_policy/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ChoronRolePolicy
+  VERSION = "0.1.0"
+end
---
+++ spec/choron_role_policy_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ChoronRolePolicy do
+  it "has a version number" do
+    expect(ChoronRolePolicy::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
